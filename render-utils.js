f5371ab7dfa514d4a9b9aa15dca6b4e6a7c4efdc
Alice Hsing <alicehsing@gmail.com>
2022-01-05 17:25:03 -0800

    added renderTodo(), createTodo(), deleteAllTodos(), getTodos(), comopleteTodo(), displayTodos() and eventlisteners for todoForm and window load

--- render-utils.js
+++ render-utils.js
@@ -1,12 +1,18 @@
 export function renderTodo(todo) {
     // create a div and a p tag
+    const todoDiv = document.createElement('div');
+    const todoP = document.createElement('p');
     // depending on whether the todo is complete, give the div the appropriate css class ('complete' or 'incomplete')
-
     // add the 'todo' css class no matter what
-
+    if (todo.complete) {
+        todoDiv.classList.add('complete', 'todo');
+    } else {
+        todoDiv.classList.add('incomplete', 'todo');
+    }
     // put the todo's text into the p tag
-
+    todoP.textContent = todo.todo;
     // append stuff
-
+    todoDiv.append(todoP);
     // return the div
+    return todoDiv;
 }
--- todos/todos.js
+++ todos/todos.js
@@ -6,6 +6,7 @@ import {
     logout,
     deleteAllTodos, 
 } from '../fetch-utils.js';
+
 import { renderTodo } from '../render-utils.js';
 
 checkAuth();
@@ -15,21 +16,48 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
+
+
 todoForm.addEventListener('submit', async(e) => {
-    // on submit, create a todo, reset the form, and display the todos
+    // on submit, create a todo item in supabase
+    e.preventDefault();
+    const data = new FormData(todoForm);
+    const todoItem = data.get('todo');
+
+    await createTodo(todoItem);
+    // reset the form
+    todoForm.reset();
+    // fetch the item and display the todos
+    await displayTodos();
 });
 
 async function displayTodos() {
     // fetch the todos
+    const todoList = await getTodos();
     
+    todosEl.textContent = '';
+    // loop through the lists
     // display the list of todos
+    for (let todo of todoList) {
+        const todoItemEl = renderTodo(todo);
+        todosEl.append(todoItemEl);
 
-    // be sure to give each todo an event listener
-
-    // on click, complete that todo
+        if (!todo.complete) {
+            // be sure to give each todo an event listener
+            todoItemEl.addEventListener('click', async() => {
+                await completeTodo(todo.id);
+                await displayTodos();
+            });
+        }
+        todosEl.append(todoItemEl);
+    }
+    
 }
 
 // add an on load listener that fetches and displays todos on load
+window.addEventListener('load', async() => {
+    await displayTodos();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
@@ -38,6 +66,7 @@ logoutButton.addEventListener('click', () => {
 
 deleteButton.addEventListener('click', async() => {
     // delete all todos
-
+    await deleteAllTodos();
     // then refetch and display the updated list of todos
+    await displayTodos();
 });
